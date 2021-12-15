d3a819023d13cdc4628158eefbd15dfdf0a68dfd
Birendra Chhetri <birendra@abit.bt>
2021-12-15 11:52:27 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.owl-1').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: false,
+        smartSpeed: 4000,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        items: 4
+    });
+});
