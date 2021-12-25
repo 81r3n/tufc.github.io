4feeab4b74dbd211601d85c5f6e95f7fc23d38c5
Birendra Chhetri <birendra@abit.bt>
2021-12-25 17:39:12 +0600

    design till date

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ $(document).ready(function() {
         autoplayTimeout: 3000,
         items: 4
     });
-
+    // loader animation
+    var tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+    tl.to(".loaderLogo", { scale: 1.05, opacity: 1, duration: 1 });
+    tl.to(".loaderText", { opacity: 1, y: 0, duration: 1 }, "-=0.5");
+    tl.to(".loader", { y: "-100%", duration: 0.5 }, "+=0.5");
     // for appear on scroll-up nav
-
     (function() {
         var doc = document.documentElement;
         var w = window;
